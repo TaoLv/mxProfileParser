f5a112cac80efe6408f8f3585693d60d686263e7
Lv Tao <tao.a.lv@intel.com>
2018-02-12 12:44:43 +0800

    fix flake8 issues

--- mxProfileParser.py
+++ mxProfileParser.py
@@ -1,21 +1,29 @@
 import json
 import argparse
 
+
 def add_args(parser):
-    parser.add_argument("--file", help="profile json file", default="profile.json")
+    parser.add_argument("--file",
+                        help="profile json file", default="profile.json")
     parser.add_argument("--op", help="operator need to anlyze", default="all")
-    parser.add_argument("--iterations", help="number of iteration, including warm up", default=0, type=int)
+    parser.add_argument("--iterations",
+                        help="number of iteration, including warm up",
+                        default=0, type=int)
+
 
 def parse_all(events, ops, cnt, dur):
     assert isinstance(events, list)
     for i in range(len(events)):
         if events[i]['name'] in ops:
             name = events[i]['name']
-            if events[i]['ph'] == 'B' and events[i+1]['name'] == name and events[i+1]['ph'] == 'E':
+            if events[i]['ph'] == 'B' and \
+               events[i+1]['name'] == name and \
+               events[i+1]['ph'] == 'E':
                 cnt[str(name)] += 1
                 dur[str(name)] += events[i+1]['ts'] - events[i]['ts']
 
     return ops, cnt, dur
+
 
 def print_all(cnt, dur, iters=0):
     print('Time of each OP:')
@@ -31,20 +39,24 @@ def print_all(cnt, dur, iters=0):
             assert cnt[name] % iters == 0
             str1 = ('%%-%ds' % maxname) % name
             str2 = ('%%-%ds ms' % maxtotal) % (dur[name] / 1000.0)
-            str3 = ('%%-%ds ms/call' % maxpercall) % (dur[name] / 1000.0 / cnt[name])
+            str3 = ('%%-%ds ms/call' % maxpercall) % \
+                (dur[name] / 1000.0 / cnt[name])
             str4 = ('%%-%ds calls' % maxcall) % cnt[name]
             str5 = '%-4s calls/iter' % (cnt[name] / iters)
             print('%s  %s \t%s \t%s \t%s' % (str1, str2, str3, str4, str5))
         else:
             str1 = ('%%-%ds' % maxname) % name
             str2 = ('%%-%ds ms' % maxtotal) % (dur[name] / 1000.0)
-            str3 = ('%%-%ds ms/call' % maxpercall) % (dur[name] / 1000.0 / cnt[name])
+            str3 = ('%%-%ds ms/call' % maxpercall) % \
+                (dur[name] / 1000.0 / cnt[name])
             str4 = ('%%-%ds calls' % maxcall) % cnt[name]
             print('%s  %s \t%s \t%s' % (str1, str2, str3, str4))
 
     print('\nTotal OP Time: %.8f ms' % (sum(dur.values()) / 1000.0))
     if iters != 0:
-        print('Iteration Time: %.8f ms\n' % (sum(dur.values()) / 1000.0 / iters))
+        print('Iteration Time: %.8f ms\n' %
+              (sum(dur.values()) / 1000.0 / iters))
+
 
 def print_op(op, cnt, dur, iters=0):
     print('Time of %s:' % op)
@@ -55,29 +67,33 @@ def print_op(op, cnt, dur, iters=0):
     maxcall = max([len(str(v)) for v in cnt.values()])
 
     for i in range(len(cnt)):
-        name = cnt.keys()[i]
+        name = list(cnt.keys())[i]
         if op in name:
             if iters != 0:
                 assert cnt[name] % iters == 0
                 str1 = ('%%-%ds' % maxname) % name
                 str2 = ('%%-%ds ms' % maxtotal) % (dur[name] / 1000.0)
-                str3 = ('%%-%ds ms/call' % maxpercall) % (dur[name] / 1000.0 / cnt[name])
+                str3 = ('%%-%ds ms/call' % maxpercall) % \
+                    (dur[name] / 1000.0 / cnt[name])
                 str4 = ('%%-%ds calls' % maxcall) % cnt[name]
                 str5 = '%-4s calls/iter' % (cnt[name] / iters)
                 print('%s  %s \t%s \t%s \t%s' % (str1, str2, str3, str4, str5))
             else:
                 str1 = ('%%-%ds' % maxname) % name
                 str2 = ('%%-%ds ms' % maxtotal) % (dur[name] / 1000.0)
-                str3 = ('%%-%ds ms/call' % maxpercall) % (dur[name] / 1000.0 / cnt[name])
+                str3 = ('%%-%ds ms/call' % maxpercall) % \
+                    (dur[name] / 1000.0 / cnt[name])
                 str4 = ('%%-%ds calls' % maxcall) % cnt[name]
                 print('%s  %s \t%s \t%s' % (str1, str2, str3, str4))
+
 
 def init_table(events):
     ops = []
     cnt = {}
     dur = {}
     for i in range(len(events)):
-        if events[i]['name'] != 'process_name' and events[i]['name'] not in ops:
+        if events[i]['name'] != 'process_name' and \
+           events[i]['name'] not in ops:
             ops.append(str(events[i]['name']))
             if events[i]['name'] not in cnt.keys():
                 cnt.update({events[i]['name']: 0})
@@ -86,9 +102,10 @@ def init_table(events):
 
     return ops, cnt, dur
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="mxnet profile file analysis",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter) # noqa
     add_args(parser)
     args = parser.parse_args()
     f = open(args.file, 'r')
